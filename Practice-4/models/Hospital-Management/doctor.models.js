5b5f57e280c7be8b43a4d5c859d34db5ab114eb4
Pramod_Acharya <pramodacharya808@gmail.com>
2024-05-12 22:34:28 +0530

    practice 5

--- Practice-4/models/Hospital-Management/doctor.models.js
+++ Practice-4/models/Hospital-Management/doctor.models.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({
-  
-}, { timestamps: true });
+const doctorSchema = new mongoose.Schema(
+  {
+    /*
+  name
+  salary
+  workingHospitals
+  qualification
+  exp
+  */
+  },
+  { timestamps: true }
+);
 
 export const Doctor = mongoose.model("Doctor", doctorSchema);
