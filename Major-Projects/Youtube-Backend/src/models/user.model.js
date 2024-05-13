8b14bdf878b689b56af63a91a43f915313f37647
Pramod_Acharya <pramodacharya808@gmail.com>
2024-05-13 21:56:45 +0530

    advance backend project

--- Major-Projects/Youtube-Backend/src/models/user.model.js
+++ Major-Projects/Youtube-Backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -44,5 +45,17 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+  }
+  return next();
+});
+
+userSchema.methods.isPasswordCorrect = function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
