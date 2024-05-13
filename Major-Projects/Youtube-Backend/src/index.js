1bda2aeb1c009cce2e49654ac143b884cdf7973e
Pramod_Acharya <pramodacharya808@gmail.com>
2024-05-13 19:24:03 +0530

    advance backend project

--- Major-Projects/Youtube-Backend/src/index.js
+++ Major-Projects/Youtube-Backend/src/index.js
@@ -1,6 +1,21 @@
 // import dotenv from "dotenv";
+// dotenv.config({ path: "./env" });
 import "dotenv/config";
 import connectDb from "./db/dbConnection.js";
-// dotenv.config({ path: "./env" });
+import { app } from "./app.js";
 
-connectDb();
+// connectDb async function will return a promise
+connectDb()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("APPLICATION ERROR .. ", err);
+      process.exit(1);
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running on port: ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR CONNECTING TO MONGODB DATABASE !!", err);
+  });
