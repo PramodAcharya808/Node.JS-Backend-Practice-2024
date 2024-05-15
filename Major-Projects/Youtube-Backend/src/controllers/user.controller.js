8f0ae464fa101dd8a1b5d5ee114392551bf8090a
Pramod_Acharya <pramodacharya808@gmail.com>
2024-05-15 18:54:20 +0530

    practice

---
+++ Major-Projects/Youtube-Backend/src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+const registerUser = (req, res) => {
+  try {
+    res.status(200).json({
+      message: "OK",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+    console.log(err.message);
+  }
+};
