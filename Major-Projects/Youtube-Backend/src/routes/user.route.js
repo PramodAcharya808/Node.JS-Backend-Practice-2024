930a893546021294271587228893b39b369faee3
Pramod_Acharya <pramodacharya808@gmail.com>
2024-05-22 20:55:32 +0530

    added router

--- Major-Projects/Youtube-Backend/src/routes/user.route.js
+++ Major-Projects/Youtube-Backend/src/routes/user.route.js
@@ -5,6 +5,12 @@ import {
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
+  currentUser,
+  updateAccountInformations,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { JWTverify } from "./../middlewares/auth.middleware.js";
@@ -31,5 +37,13 @@ router.route("refreshaccesstoken").post(refreshAccessToken);
 // secured routes
 router.route("/logout").post(JWTverify, logoutUser);
 router.route("/changepassword").post(JWTverify, changeCurrentPassword);
+router.route("/currentUser").get(JWTverify, currentUser);
+router.route("/update-details").patch(JWTverify, updateAccountInformations);
+router.route("/avatar").patch(JWTverify, upload.single("avatar"), updateAvatar);
+router
+  .route("/cover-image")
+  .patch(JWTverify, upload.single("coveImage"), updateCoverImage);
+router.route("/c/:username").get(JWTverify, getUserChannelProfile);
+router.route("/history").get(JWTverify, getWatchHistory);
 
 export default router;
